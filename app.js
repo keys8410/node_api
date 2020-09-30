382b0eea4a84e62a7564f65501097da76a0d337b
keys8410 <yanalmeidagarcia@gmail.com>
2020-09-30 00:51:30 -0300

    Melhorando o projeto e tratando erros

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
-const routeProducts = require('./routes/produtos');
+const routeProdutos = require('./routes/produtos');
+const routePedidos = require('./routes/pedidos');
 
-app.use('/produtos', routeProducts);
+app.use(morgan('dev'));
+
+app.use('/produtos', routeProdutos);
+app.use('/pedidos', routePedidos);
+
+app.use((req, res, next) => {
+  const error = new Error('Nenhuma rota encontrada.');
+  error.status = 400;
+
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+
+  return res.send({
+    error: { message: error.message },
+  });
+});
 
 module.exports = app;
